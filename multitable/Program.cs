ad51763d6e6ba7966c3727ae44b167ae06619d76
Сергей Макаров <sergey.mcarrow@gmail.com>
2018-03-08 12:21:13 +0200

    mt

--- multitable/Program.cs
+++ multitable/Program.cs
@@ -10,6 +10,39 @@ namespace multitable
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите количество примеров:");
+            int count = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int countcorrect = 0;
+            int countwrong = 0;
+            int countincorrect = 0;
+            for (int i = 1; i <= count ; i++)
+            {
+                    int n1 = rnd.Next(2, 10);
+                    int n2 = rnd.Next(2, 10);
+                    Console.Write($"{n1} * {n2} = ");
+                    string input = (Console.ReadLine());
+                    int answer;
+                    bool parsed = int.TryParse(input, out answer);
+                if (!parsed)
+                {
+                    Console.WriteLine("Повторите ввод.Вводить можно только цифры");
+                    countincorrect++;
+                    continue;
+                }
+                else if
+                (n1 * n2 == answer)
+                {
+                    Console.WriteLine("Молодец");
+                    countcorrect++;
+                }
+                else
+                {
+                    Console.WriteLine("Тупица");
+                    countwrong++;
+                }
+            }
+            Console.WriteLine($"Правильных ответов: {countcorrect}, а неправльных:{countwrong}. Некорректный ввод : {countincorrect}раз");
         }
     }
 }
